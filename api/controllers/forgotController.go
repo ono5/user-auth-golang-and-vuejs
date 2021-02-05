c170d01111a97a5603c47f06a09d616fda2cfded
ono5 <kuroneko_head_lord@yahoo.co.jp>
2021-02-05 10:58:21 +0900

    feat: Added Email sending feature

--- api/controllers/forgotController.go
+++ api/controllers/forgotController.go
@@ -4,7 +4,9 @@ package controllers
 import (
 	"auth-api/database"
 	"auth-api/models"
+	"fmt"
 	"math/rand"
+	"net/smtp"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -25,6 +27,29 @@ func Forgot(c *fiber.Ctx) error {
 
 	// DBに保存
 	database.DB.Create(&passwordReset)
+
+	// SMTPメール送信
+	from := "selfnote-owner@yahoo.co.jp"
+	to := []string{
+		data["email"],
+	}
+	sendFrom := fmt.Sprintf("From: %s\n", from)
+	subject := fmt.Sprintf("Subject; %s\n", "Password Reset")
+	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+	// Vue.jsのアドレス
+	url := "http://localhost:8080/reset/" + token
+	message := fmt.Sprintf("Click <a href=\"%s\">here</a> to reset password!", url)
+	err := smtp.SendMail(
+		"smtp:1025", // コンテナサービス名+port
+		nil,
+		from,
+		to,
+		[]byte(sendFrom+subject+mime+message),
+	)
+
+	if err != nil {
+		return err
+	}
 
 	return c.JSON(fiber.Map{
 		"message": "SUCCESS",
