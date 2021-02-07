0a1fdbe65975b516b7e970c2dd3991c111f27138
ono5 <kuroneko_head_lord@yahoo.co.jp>
2021-02-08 07:02:38 +0900

    feat: Added authentication feature

--- ui/src/pages/Home.vue
+++ ui/src/pages/Home.vue
@@ -1,11 +1,33 @@
 //  Home.vue
 <template>
-Home
+  <div class="container">
+    <h1>{{ message }}</h1>
+  </div>
 </template>
 
 <script>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
 export default {
-  name: "Home"
+  name: "Home",
+  setup() {
+    const message = ref('You are not logged in!')
+
+    onMounted(async () => {
+      // user情報を取得
+      // ログイン情報は、Cookieに保存してあるので、
+      // リクエストするだけでOK
+      const { data } = await axios.get('user')
+
+      message.value = `Hi ${data.first_name} ${data.last_name}`
+    })
+
+    return {
+      message
+    }
+  }
+
 }
 </script>
 
